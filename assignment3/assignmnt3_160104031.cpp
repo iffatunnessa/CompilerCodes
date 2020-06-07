0ed466c36ae9ff201de6ef5579bd8067cff23c90
iffatunnessa <45840519+iffatunnessa@users.noreply.github.com>
2020-06-07 23:50:48 +0600

    a

---
+++ assignment3/assignmnt3_160104031.cpp
@@ -0,0 +1,109 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+struct symbol{
+    int aValue;
+    char name[20];
+    char type[5];
+    char datatype[10];
+    char scope[5];
+
+}sym[10];
+
+int main(void){
+    FILE *p1, *p2;
+    char c,c1,lex[20];
+    int i=0, j=0;
+    int value = 0;
+    p1 = fopen("scanned.txt", "r");
+    p2 = fopen("new.txt", "w");
+
+    if(!p1) {
+        printf("\nFile can't be opened!");
+    }
+    else{
+        while((c = fgetc(p1)) !=EOF){
+            if(c == '['){
+                c1 = fgetc(p1);
+                fputc(c,p2);
+                if(c1!= 'i'){
+                    while(c != ' '){
+                        c = c1;
+                        c1 = fgetc(p1);
+                    }
+                    while(c != ']'){
+                        c = c1;
+                        c1 = fgetc(p1);
+                        fputc(c,p2);
+                    }
+                }
+                else if(c1 == 'i'){
+                    while(c != ']'){
+                        c = c1;
+                        c1 = fgetc(p1);
+                        fputc(c,p2);
+                    }
+                }
+
+            }
+
+        }
+    }
+    fclose(p1);
+    fclose(p2);
+
+
+
+    p2 = fopen("new.txt","r");
+    while((c=fgetc(p2))!=EOF)
+        printf("%c",c);
+    fclose(p2);
+    printf("\n\n");
+
+    p1 = fopen("new.txt", "r");
+
+    if(!p1) {
+        printf("\nFile can't be opened!");
+    }
+    else{
+        while((c = fgetc(p1)) !=EOF){
+            if(c == '['){
+                c1 = fgetc(p1);
+                int k = 0;
+                while(c1 != ']'){
+
+                    sym[j].datatype[k] = c1;
+                    k++;
+                    c = c1;
+                    c1 = fgetc(p1);
+                    //printf("%c",sym[j].datatype[k]);
+                }
+            }
+            else if(c1 == 'i'){
+                c = c1;
+                c1 = fgetc(p1);
+                sym[j].aValue = value++;
+                while(c1 != ']'){
+                    int k = 0;
+                    sym[j].name[k] = c;
+                    k++;
+                    c = c1;
+                    c1 = fgetc(p1);
+                }
+            }
+                else{
+                    c = c1;
+                    c1 = fgetc(p1);
+                }
+
+        j++;
+        }
+    }
+    fclose(p1);
+    int r;
+    cout << sym[0].datatype;
+    printf("\n\n");
+
+}
+
